a8acc57e6100ee38b755a3329765734a26daaa33
BHAVYA BANSAL <117255729+BhavyaBansal1@users.noreply.github.com>
2026-01-28 17:10:48 +0530

    Add files via upload

---
+++ Advance/libraryclient/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace kitchen;//refrence from another class
+class libraryuser
+{
+    public static void Main()
+    {
+        veg v=new veg();
+        var ans=v.food();
+      var ans1= v.phonenumber();
+        Console.WriteLine(ans+" "+ans1);
+    }
+
+}
---
+++ Advance/mylibrary/kitchen.cs
@@ -0,0 +1,14 @@
+namespace kitchen{
+   public  class veg
+   {        
+    public string food(){
+        return "food is ready...";
+    }
+    public int phonenumber(){
+        return 1234;
+    }
+
+   }            
+}
+
+
